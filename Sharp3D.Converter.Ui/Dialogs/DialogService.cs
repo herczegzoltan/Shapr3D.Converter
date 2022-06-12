0707ed105b010aaa4bda3e5970e226e9f0ff594b
DESKTOP-8N0UB1O\Zoltan <herczeg.zoltan92@gmail.com>
2022-06-12 15:02:23 +0200

    Add DialogService

---
+++ Sharp3D.Converter.Ui/Dialogs/DialogService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Sharp3D.Converter.Ui.Dialogs
+{
+    public interface IDialogService
+    {
+        Task<bool?> ShowBlockingQuestionModalDialog(string tilte, string description, string primary = "Yes", string secondary = "No");
+        Task ShowExceptionModalDialog(Exception ex, string description = null);
+    }
+
+    public class DialogService : ContentDialog, IDialogService
+    {
+        public async Task ShowExceptionModalDialog(Exception ex, string description = null)
+        {
+            try
+            {
+                Title = "Exception thrown";
+                Content = description;
+                CloseButtonText = "Ok";
+
+                await ShowAsync();
+            }
+            catch (Exception)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<bool?> ShowBlockingQuestionModalDialog(string tilte, string description, string primary = "Yes", string secondary = "No")
+        {
+            try
+            {
+                Title = tilte;
+                Content = description;
+                PrimaryButtonText = primary;
+                CloseButtonText = secondary;
+
+                var result = await ShowAsync();
+
+                return result == ContentDialogResult.Primary;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
