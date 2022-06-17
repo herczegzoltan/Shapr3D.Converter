4f6ce5f3a86ea37f4703092357b0c6a7a3fa99d6
DESKTOP-8N0UB1O\Zoltan <herczeg.zoltan92@gmail.com>
2022-06-17 11:48:22 +0200

    Remove throw ex; from DialogService

--- Sharp3D.Converter.Ui/Dialogs/DialogService.cs
+++ Sharp3D.Converter.Ui/Dialogs/DialogService.cs
@@ -9,89 +9,68 @@ namespace Sharp3D.Converter.Ui.Dialogs
     {
         public async Task ShowExceptionModalDialog(Exception ex, string description = null, string buttonContent = "Ok")
         {
-            try
+            var title = "Exception thrown";
+            var content = $"{ex.Message} {description}";
+            var cancelCommand = new UICommand(buttonContent, cmd => { });
+
+            var dialog = new MessageDialog(content, title)
             {
-                var title = "Exception thrown";
-                var content = $"{ex.Message} {description}";
-                var cancelCommand = new UICommand(buttonContent, cmd => {});
+                Options = MessageDialogOptions.None,
+                DefaultCommandIndex = 0,
+                CancelCommandIndex = 0
+            };
 
-                var dialog = new MessageDialog(content, title)
-                {
-                    Options = MessageDialogOptions.None,
-                    DefaultCommandIndex = 0,
-                    CancelCommandIndex = 0
-                };
-
-                if (cancelCommand != null)
-                {
-                    dialog.Commands.Add(cancelCommand);
-                    dialog.CancelCommandIndex = (uint)dialog.Commands.Count - 1;
-                }
-
-                _ = await dialog.ShowAsync();
-            }
-            catch (Exception)
+            if (cancelCommand != null)
             {
-                throw ex;
+                dialog.Commands.Add(cancelCommand);
+                dialog.CancelCommandIndex = (uint)dialog.Commands.Count - 1;
             }
+
+            _ = await dialog.ShowAsync();
         }
 
         public async Task<bool?> ShowBlockingQuestionModalDialog(string title, string description, string primary = "Yes", string secondary = "No")
         {
-            try
+            var primaryCommand = new UICommand(primary, cmd => { });
+            var secondaryCommand = new UICommand(secondary, cmd => { });
+
+            var dialog = new MessageDialog(description, title)
             {
-                var primaryCommand = new UICommand(primary, cmd => { });
-                var secondaryCommand = new UICommand(secondary, cmd => { });
+                Options = MessageDialogOptions.None,
+                DefaultCommandIndex = 0,
+                CancelCommandIndex = 0
+            };
+            dialog.Commands.Add(primaryCommand);
 
-                var dialog = new MessageDialog(description, title)
-                {
-                    Options = MessageDialogOptions.None,
-                    DefaultCommandIndex = 0,
-                    CancelCommandIndex = 0
-                };
-                dialog.Commands.Add(primaryCommand);
-
-                if (secondaryCommand != null)
-                {
-                    dialog.Commands.Add(secondaryCommand);
-                    dialog.CancelCommandIndex = (uint)dialog.Commands.Count - 1;
-                }
-
-                var command = await dialog.ShowAsync();
-
-                return command == primaryCommand;
-            }
-            catch (Exception ex)
+            if (secondaryCommand != null)
             {
-                throw ex;
+                dialog.Commands.Add(secondaryCommand);
+                dialog.CancelCommandIndex = (uint)dialog.Commands.Count - 1;
             }
+
+            var command = await dialog.ShowAsync();
+
+            return command == primaryCommand;
         }
 
         public async Task ShowOkModalDialog(string title, string description, string buttonContent = "Ok")
         {
-            try
+            var cancelCommand = new UICommand(buttonContent, cmd => { });
+
+            var dialog = new MessageDialog(description, title)
             {
-                var cancelCommand = new UICommand(buttonContent, cmd => { });
+                Options = MessageDialogOptions.None,
+                DefaultCommandIndex = 0,
+                CancelCommandIndex = 0
+            };
 
-                var dialog = new MessageDialog(description, title)
-                {
-                    Options = MessageDialogOptions.None,
-                    DefaultCommandIndex = 0,
-                    CancelCommandIndex = 0
-                };
-
-                if (cancelCommand != null)
-                {
-                    dialog.Commands.Add(cancelCommand);
-                    dialog.CancelCommandIndex = (uint)dialog.Commands.Count - 1;
-                }
-
-                _ = await dialog.ShowAsync();
-            }
-            catch (Exception ex)
+            if (cancelCommand != null)
             {
-                throw ex;
+                dialog.Commands.Add(cancelCommand);
+                dialog.CancelCommandIndex = (uint)dialog.Commands.Count - 1;
             }
+
+            _ = await dialog.ShowAsync();
         }
     }
 }
