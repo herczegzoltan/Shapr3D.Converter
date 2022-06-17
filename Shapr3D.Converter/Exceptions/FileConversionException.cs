1e008bfb5e9ef86c581bac732eed71c7c4e7d862
DESKTOP-8N0UB1O\Zoltan <herczeg.zoltan92@gmail.com>
2022-06-17 11:47:56 +0200

    Add FileConversionException to use in ApplyConverterAndReportAsync

---
+++ Shapr3D.Converter/Exceptions/FileConversionException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Shapr3D.Converter.Extensions
+{
+    [Serializable]
+    public class FileConversionException : Exception
+    {
+        public FileConversionException()
+        {
+        }
+
+        public FileConversionException(string message) : base(message)
+        {
+        }
+
+        public FileConversionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected FileConversionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
